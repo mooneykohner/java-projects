4d84596dc6514599879b5deb1078882ed57130b5
mooneykohner <mooneykohner@gmail.com>
2024-11-13 17:40:42 -0600

    Added ratio to CoinFlip

--- src/CoinFlip.java
+++ src/CoinFlip.java
@@ -3,16 +3,18 @@ import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.media.*;
 import javafx.stage.*;
 
 public class CoinFlip extends Application { // Extends class through JavaFX application.
 
-    private int heads = 0;
-    private int tails = 0;
+    private int heads = 0; // Initializes variable for tracking amount of times flip results in heads.
+    private int tails = 0; // Initializes variable for tracking amount of times flip results in tails.
+
+    private int coinsFlipped = 0; // Initializes variable for the amount of times a coin is flipped.
+
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); // Launches the application window.
     }
 
     @Override // Overrides default method from the Application class.
@@ -34,21 +36,17 @@ public class CoinFlip extends Application { // Extends class through JavaFX appl
 
         Label headsAmountLabel = new Label("");
         Label tailsAmountLabel = new Label("");
+        Label coinsFlippedLabel = new Label("");
 
-        flipCoinButton.setOnAction(e -> flipCoin(flipCoinResultLabel, headsAmountLabel, tailsAmountLabel));
+        flipCoinButton.setOnAction(e -> flipCoin(flipCoinResultLabel, headsAmountLabel, tailsAmountLabel, coinsFlippedLabel));
 
-        VBox layout = new VBox(10, flipCoinResultLabel, flipCoinButton,  headsAmountLabel, tailsAmountLabel);
-        layout.setStyle("-fx-padding: 20px; -fx-alignment: center; -fx-font-size: 50px;");
-        return layout;
+        VBox layout = new VBox(10, flipCoinResultLabel, flipCoinButton,  headsAmountLabel, tailsAmountLabel, coinsFlippedLabel);
+        layout.setStyle("-fx-padding: 20px; -fx-alignment: center; -fx-font-size: 50px;"); // CSS styling for VBox
+        return layout; // Returns layout for Scene.
     }
 
-    public void flipCoin(Label flipCoinResultLabel, Label headsAmountLabel, Label tailsAmountLabel) {
+    public void flipCoin(Label flipCoinResultLabel, Label headsAmountLabel, Label tailsAmountLabel, Label coinsFlippedLabel) {
         int random = (int) (Math.random() * 2) + 1;
-
-        Media coinFlipSound = new Media(getClass().getResource("coinflip.mp3").toExternalForm());
-        MediaPlayer coinFlipSoundPlayer = new MediaPlayer(coinFlipSound);
-        coinFlipSoundPlayer.play();
-
 
         if (random == 1) {
             flipCoinResultLabel.setText("Heads!");
@@ -57,8 +55,14 @@ public class CoinFlip extends Application { // Extends class through JavaFX appl
             flipCoinResultLabel.setText("Tails!");
             tails++;
         }
+        coinsFlipped++;
+
+        double coinsFlippedHeadsRatio = Math.floor((double) heads / coinsFlipped * 100);
+        double coinsFlippedTailsRatio = Math.floor((double) tails / coinsFlipped * 100);
+
         headsAmountLabel.setText("Heads: " + heads);
         tailsAmountLabel.setText("Tails: " + tails);
+        coinsFlippedLabel.setText(coinsFlippedHeadsRatio + "%" + " / " + coinsFlippedTailsRatio + "%");
     }
 }
 
